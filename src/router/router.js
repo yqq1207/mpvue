e6bff63e515914c2bc0682ce86c077fdc5e2e14c
yqqzxc <qiqi.yu@mail.geeu.tech>
2018-10-11 10:36:56 +0800

    eee

---
+++ src/router/router.js
@@ -0,0 +1,58 @@
+module.exports = [
+    {
+      path: '/pages/index/index',
+      config: {
+        navigationBarTitleText: '町盟',
+        enablePullDownRefresh: true
+      }
+    },
+    {
+      path: '/pages/item/index',
+      config: {
+        navigationBarTitleText: '商品详情',
+        enablePullDownRefresh: true
+      }
+    },
+    {
+      path: '/pages/item/pay',
+      config: {
+        navigationBarTitleText: '町盟',
+        enablePullDownRefresh: true
+      }
+    },
+    {
+      path: '/pages/item/payError',
+      config: {
+        navigationBarTitleText: '町盟',
+        enablePullDownRefresh: true
+      }
+    },
+    {
+      path: '/pages/item/joinError',
+      config: {
+        navigationBarTitleText: '町盟',
+        enablePullDownRefresh: true
+      }
+    },
+    {
+      path: '/pages/item/paySuccess',
+      config: {
+        navigationBarTitleText: '町盟',
+        enablePullDownRefresh: true
+      }
+    },
+    {
+      path: '/pages/mine/index',
+      config: {
+        navigationBarTitleText: '町盟',
+        enablePullDownRefresh: true
+      }
+    },
+    {
+      path: '/pages/mine/detail',
+      config: {
+        navigationBarTitleText: '町盟',
+        enablePullDownRefresh: true
+      }
+    },
+  ]
